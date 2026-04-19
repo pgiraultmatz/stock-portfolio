807d9e9a63a818c0a512357dbe2a7428dc1abe22
pgiraultmatz <pgiraultmatz.pro@gmail.com>
2026-04-19 18:16:15 +0200

    Limit variations refresh

--- main.go
+++ main.go
@@ -628,7 +628,26 @@ func (s *Server) routes() http.Handler {
 	})
 
 	mux.Handle("/", s.requireAuth(protected))
-	return mux
+	return logRequests(mux)
+}
+
+func logRequests(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rw := &responseWriter{ResponseWriter: w, status: http.StatusOK}
+		start := time.Now()
+		h.ServeHTTP(rw, r)
+		log.Printf("%s %s %d %s", r.Method, r.URL.Path, rw.status, time.Since(start).Round(time.Millisecond))
+	})
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
 }
 
 // ---- misc ----
